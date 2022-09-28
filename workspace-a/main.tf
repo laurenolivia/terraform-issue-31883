63cba26120e3af0107e1b6127dc94a7313ab9b4b
Lauren <lauren.burwell@hashicorp.com>
2022-09-28 15:05:55 -0400

    reproduce null vals

---
+++ workspace-a/main.tf
@@ -0,0 +1,40 @@
+terraform {
+  cloud {
+      hostname = "tfcdev-2c13224a.ngrok.io"
+      organization  = "hashicorp-31883-a"
+      workspaces {
+        name = "test-31883-x"
+      }
+
+  }
+  required_providers {
+    tfe = {
+      version = "~> 0.35.0"
+    }
+  }
+}
+
+provider "tfe" {
+  hostname = "tfcdev-2c13224a.ngrok.io"
+  token    = ""
+}
+
+resource "tfe_organization" "test" {
+  name  = "hashicorp-31883-b"
+  email = "lauren.burwell@hashicorp.com"
+}
+
+resource "tfe_workspace" "test-workspace" {
+  name         = "test-31883-c"
+  organization = tfe_organization.test.name
+}
+
+output "rgp" {
+  value = {
+    id   = tfe_workspace.test-workspace.id
+    name = tfe_workspace.test-workspace.name
+
+  }
+    sensitive = true
+
+}
---
+++ workspace-b/main.tf
@@ -0,0 +1,29 @@
+terraform {
+  required_providers {
+    tfe = {
+      version = "~> 0.35.0"
+    }
+  }
+}
+
+provider "tfe" {
+  hostname = "tfcdev-2c13224a.ngrok.io"
+  token    = ""
+}
+
+data "tfe_outputs" "A" {
+  organization = "hashicorp-31883-a"
+  workspace    = "test-31883-x"
+}
+
+output "test1" {
+  value = {
+    rgp_name = nonsensitive(data.tfe_outputs.A.values)
+    rgp_id = nonsensitive(data.tfe_outputs.A.values)
+  }
+}
+
+output "test2" {
+  value = data.tfe_outputs.A.values
+  sensitive = true
+}
